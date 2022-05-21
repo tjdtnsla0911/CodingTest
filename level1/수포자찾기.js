b00160725483fbddc7549ef5a3cdd2f28f30812e
KIM SUNGSOO <62128928+tjdtnsla0911@users.noreply.github.com>
2022-05-21 20:38:58 +0900

    이거좀 짜증남

--- level1/수포자찾기.js
+++ level1/수포자찾기.js
@@ -3,7 +3,7 @@ const solution = (answers) => {
     obj.member1 = [1, 2, 3, 4, 5, 1, 2, 3, 4, 5];
     obj.member2 = [2, 1, 2, 3, 2, 4, 2, 5, 2, 1, 2, 3, 2, 4, 2, 5];
     obj.member3 = [3, 3, 1, 1, 2, 2, 4, 4, 5, 5, 3, 3, 1, 1, 2, 2, 4, 4, 5, 5];
-    
+     
     let result = [0,0,0];
     let returnValue = [];
     

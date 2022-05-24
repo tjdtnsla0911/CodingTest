5dafc954276d13bfb9980140d290b0a994f4fbae
KIM SUNGSOO <62128928+tjdtnsla0911@users.noreply.github.com>
2022-05-24 21:02:23 +0900

    이것이 한방코드다

---
+++ level1/K번째수.js
@@ -0,0 +1,7 @@
+const solution = (array = '한방코드 ㅍㅌㅊ?', commands,result) =>{
+    
+    return result = commands.map((v,i) => { return array.slice((v[0]-1),v[1]).sort((k,j)=>{ return k - j})[v[(v.length)-1]-1]; });
+      
+ }
+ 
+ solution([1, 5, 2, 6, 3, 7, 4],[[2, 5, 3], [4, 4, 1], [1, 7, 3]],[])

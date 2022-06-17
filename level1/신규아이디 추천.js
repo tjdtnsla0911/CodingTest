cf8c5b966e1fd27f47b5d8aa29a6eac39068e052
KIM SUNGSOO <62128928+tjdtnsla0911@users.noreply.github.com>
2022-06-17 23:29:06 +0900

    하는중

--- level1/신규아이디 추천.js
+++ level1/신규아이디 추천.js
@@ -4,6 +4,7 @@ function solution(new_id) {
     let checkComma = [];
     let checkDuple = '';
     let changeDuple = [];
+    let checkLevel3 = [];
     //1단계 소문자 치환
     new_id = new_id.toLowerCase();
     
@@ -16,28 +17,41 @@ function solution(new_id) {
     //3단계 new_id에서 마침표(.)가 2번 이상 연속된 부분을 하나의 마침표(.)로 치환합니다.
     answer.split('').forEach((v,i)=>{
         if(v === answer[i+1]){
-
+            console.log('같다!!',v,i);
           checkComma = [...checkComma,i];
           checkComma = [...checkComma,i+1];
     
         }
     });
-
-    //3단계 + 중복제거 시키고 전꺼랑 비교해서 이제 치환해야함
+    console.log('[...new Set(checkComma)] :',[...new Set(checkComma)]);
+    //3단계 +중복제거 시키고 전꺼랑 비교해서 이제 치환해야함
+    checkLevel3 = Array.from(answer);
+    let dataCheck = 0;
     [...new Set(checkComma)].forEach((v,i)=>{ 
-
-            if(Array.from(answer)[v] === Array.from(answer)[v+1]){
+            console.log('@@@@@@checkLevel3 = ',checkLevel3);
+            if(checkLevel3[v] === checkLevel3[v+1]){
                 changeDuple = [... changeDuple , v];
                 changeDuple = [... changeDuple , v+1]; //걍일단 중복 시키고 밑에서 제거하자
             }else{
-                
-                console.log('changeDuple = ', [...new Set(changeDuple)]);
+                4,5/
+                6,7
             
+                console.log('###changeDuple = ',[...new Set(changeDuple)]);
+                
+               [...new Set(changeDuple)].reverse().forEach((v,i)=>{
+                        if(Math.min(...[...new Set(changeDuple)]) !== v){
+                            console.log('다르다 v = ',v);
+                            checkLevel3.splice(v,1)
+                        } 
+                })
+                changeDuple = [];
+                
             }
         
     })
 
-    return answer;
+    console.log('checkLevel3 마지막 = ',checkLevel3.join(''));
+    // return answer;
 }
 
 solution(`...!@BaT#*..y.abcdefghijklm`);
